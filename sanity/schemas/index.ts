2061e7eed5c70ca9742a4a9cec042dcf6ab6de8f
Sean G <seanguillermo@gmail.com>
2026-06-14 05:36:10 +0800

    feat: register homepage schema

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,7 +1,8 @@
+import { home } from "./home";
 import { page } from "./page";
 import { siteSettings } from "./siteSettings";
 import { post } from "./post";
 import { service } from "./service";
 import { testimonial } from "./testimonial";
 
-export const schemaTypes = [siteSettings, page, post, service, testimonial];
+export const schemaTypes = [siteSettings, home, page, post, service, testimonial];
